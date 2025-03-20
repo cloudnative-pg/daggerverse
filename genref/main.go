115cdc68c86aa3f6321b71d386cdde7b113b6f96
Niccolò Fei <niccolo.fei@enterprisedb.com>
2025-03-20 14:50:55 +0100

    feat: add a genref module
    This module allows running genref to generate
    API documentation from a given project.
    
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>

---
+++ genref/main.go
@@ -0,0 +1,66 @@
+// A generated module for Genref functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"dagger/genref/internal/dagger"
+)
+
+type Genref struct {
+	// +private
+	Ctr *dagger.Container
+}
+
+func New(
+	// genref version to use.
+	// +optional
+	// renovate: datasource=go depName=github.com/kubernetes-sigs/reference-docs/genref versioning=semver
+	// +default="015aaac611407c4fe591bc8700d2c67b7521efca"
+	genrefVersion string,
+	// +optional
+	// renovate image: datasource=docker depName=golang versioning=docker
+	// +default="golang:1.24.1-bookworm"
+	goImage string,
+	// +optional
+	ctr *dagger.Container,
+) *Genref {
+	if ctr != nil {
+		return &Genref{
+			Ctr: ctr,
+		}
+	}
+	return &Genref{
+		Ctr: dag.Container().From(goImage).
+			WithExec([]string{"go", "install",
+				fmt.Sprintf("github.com/kubernetes-sigs/reference-docs/genref@%v", genrefVersion)}),
+	}
+}
+
+func (m *Genref) Genref(
+	ctx context.Context,
+	// The directory of the repository.
+	source *dagger.Directory,
+	// +optional
+	// A list of arguments to pass to genref.
+	args []string,
+) *dagger.Container {
+	return m.Ctr.
+		WithMountedDirectory("/src", source).
+		WithWorkdir("/src").
+		WithExec(append([]string{"genref"}, args...))
+}
